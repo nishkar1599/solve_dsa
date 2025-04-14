3cec7a790b2312e4652ecf12cdf81039df30e3d9
Nishikanta <nishkar2016@gmail.com>
2025-04-14 15:48:52 +0530

    Add multiply.cpp for basic multiplication functionality

---
+++ number_theory/multiply.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+void solve(){
+    int x;
+    cin >> x;
+    cout << 10*x << endl;
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); // for faster input/output
+    cin.tie(NULL); // untie cin and cout
+    cout.tie(NULL); // untie cout and cin
+
+    int t ;
+    cin>>t;
+    while(t--){// run test cases t times
+        solve();
+    }
+    return 0;
+
+}
+
+
