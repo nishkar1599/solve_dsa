de7c609173b79e8a0e1c2894697a27365336f5e9
Nishikanta <nishkar2016@gmail.com>
2025-04-16 11:56:45 +0530

    Implement Sieve of Eratosthenes algorithm in seive.cpp

---
+++ number_theory/sieve_method/seive.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<(b); i+=k)
+#define RFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define RFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+
+void solve(int n){
+    
+    bool prime[n+1];
+    FOR(i,0,n+1){
+        prime[i]=true;  // setting all values to true
+    }
+    prime[0]=prime[1]=false;
+    FOR(i,2,n+1){
+     if(prime[i]==true){
+         FORk(j,2*i,n+1,i){
+            prime[j]=false;
+
+        }
+
+    }
+   }
+   FOR(i,0,n+1) cout<<prime[i]<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); // for faster input/output
+    cin.tie(NULL); // untie cin and cout
+    cout.tie(NULL); // untie cout and cin
+
+    /*int t ;
+    cin>>t;
+    while(t--){// run test cases t times
+        solve();
+    }
+    return 0;*/
+    
+     // 1 and 0 are not prime 
+    int N = 25;
+    solve(N);
+    return 0;
+
+
+
+
+}
+
+
