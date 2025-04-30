214d1c672a05ff0ad871e355f3211c3debb22769
Nishikanta <nishkar2016@gmail.com>
2025-04-30 12:18:41 +0530

    Refactor segmented sieve implementation for clarity and efficiency

--- number_theory/sieve_method/segmented_sieve.cpp
+++ number_theory/sieve_method/segmented_sieve.cpp
@@ -1,70 +1,77 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define FOR(i,a,b) for(int i=(a); i<(b); i++)
-#define FORk(i,a,b,k) for(int i=(a); i<(b); i+=k)
-#define RFOR(i,a,b) for(int i=(a); i>=(b); i--)
-#define RFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
-#define pb push_back
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef long long int ll;
-typedef unsigned long long int  ull;
-typedef vector<ll> vll;
-typedef vector<ull> vull;
-const int msm = 31623;
-bool pr[msm+1], p[100001];
-vi prs;
-void sieve() {
-    // Step 1
-    FOR(i, 2, msm+1)
-        pr[i] = true;
-    for(int i=2; i*i<=msm; i++)
-        if(pr[i])
-            FORk(j, i*i, msm+1, i)
-                pr[j] = false;
-    FOR(i, 2, msm+1)
-        if(pr[i])
-            prs.pb(i);
-}
-void seg_sieve(int N, int M) {
-    // Step 2 & 3
-    FOR(i,0,M-N+1)  p[i] = true;
-    if(N==1) p[0] = false; // Border case
-    int sm = int(sqrt(M));
-    for(auto i: prs) {
-        if(i>sm)
-            break;
-        int x = (N/i)*i;
-        if(x < N)
-            x += i;
-        if(x < i*i)
-            x = i*i;
-        FORk(j, x, M+1, i) {
-            p[j-N] = false;
-        }
+vector<int> primelist;
+void sieve(){
+    const int N = 100000;
+    bool prime[N+1];
+    for(int i = 0 ; i <= N ; i++){
+        prime[i] = true; // setting all values to true
+
     }
-    FOR(i, 0, M-N+1)
-        if(p[i])
-            cout << N+i << endl;
+    prime[0] = prime[1] = false; // 1 and 0 are not prime
+    int sq = sqrt(N);
+    for(int i = 2 ; i <= sq ; i++){
+        if(prime[i]==true){
+            for(int j = i*i ; j <= N; j = j+i ){
+                prime[i]=false;
+
+            }
+
+
+        }
+
+    }
+    //fetch all prime
+    
+    for (int i = 0; i < N+1; i++)
+    {
+      if(prime[i]){
+        primelist.push_back(i);
+
+      }
+    }
 }
 
-void solve() {
-    int n,m;
-    cin >> n >> m;
-    seg_sieve(n,m);
-    cout << endl;
-}
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int t = 1;
-    cin >> t;
-    sieve();
-    while(t--) {
-        solve();
+void dummy(int m , int n ){
+    int arr[n-m+1];
+    for(int i = 0 ; i <n-m+1 ; i++){
+        arr[i]= true;
+
     }
-    return 0;
+    for(int i = 0 ; i<primelist.size(); i++){
+        int p = primelist[i];
+        int start = m ;
+        if(start % p != 0){
+            start = (start/p)*p + p;
+        }
+        else{
+            start = m;
+        }
+        for(int j = start ; j <= n ; j+=p){
+            if(j != p){
+                arr[j-m] = false;
+                cout<<j<<endl;
+            }
+        }
+
+
+
+
+    }
+
+}
+
+int main() {
+	
+	int t ;
+    cin>>t;
+    sieve();//generates all prime nuber up to 10^6
+    while(t--){
+        int m ,n;
+        cin>>m>> n;
+        dummy(m,n);
+
+    }
+
+	return 0;
 }
