3904979d9c282fe7406da313e356ed057b1a3eaf
Nishikanta <nishkar2016@gmail.com>
2025-05-18 11:45:25 +0530

    code force qn on reversing array and leetcode qn based on dutch national flag algo

---
+++ arrays/reverse_arr.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    int i =0;
+    int j = n-1;
+    while(i<j){
+        int temp;
+        temp = arr[i];
+        arr[i]= arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+
+    }
+    for(int i = 0 ; i < n ; i++){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+
+
+}
